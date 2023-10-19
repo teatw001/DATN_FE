2dd56819e8f3b702d55c18fadbd065b311c9ba85
teatw001 <duydhph26793@fpt.edu.vn>
2023-10-20 00:51:34 +0700

    ...

--- src/pages/Admin/Quản Lí suất chiếu/EditShow.tsx
+++ src/pages/Admin/Quản Lí suất chiếu/EditShow.tsx
@@ -3,15 +3,15 @@ import { EditOutlined } from "@ant-design/icons";
 
 import { useNavigate } from "react-router-dom";
 import {
-    Button,
-    Col,
-    DatePicker,
-    Drawer,
-    Form,
-    Row,
-    Select,
-    Space,
-    message,
+  Button,
+  Col,
+  DatePicker,
+  Drawer,
+  Form,
+  Row,
+  Select,
+  Space,
+  message,
 } from "antd";
 
 import { useFetchProductQuery } from "../../../service/films.service";
@@ -21,167 +21,178 @@ import { useUpdateShowTimeMutation } from "../../../service/show.service";
 import moment from "moment";
 
 interface DataType {
-    id: string;
-    date: Date;
-    time_id: string;
-    film_id: string;
-    room_id: string;
+  id: string;
+  date: Date;
+  time_id: string;
+  film_id: string;
+  room_id: string;
 }
 interface EditShowProps {
-    dataShow: DataType;
+  dataShow: DataType;
 }
 
 const EditShow: React.FC<EditShowProps> = ({ dataShow }) => {
-    const [updateShowTime] = useUpdateShowTimeMutation();
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const { Option } = Select;
-    const { data: films } = useFetchProductQuery();
-    const { data: times } = useFetchTimeQuery();
-    useEffect(() => {
-        if (dataShow) {
-            form.setFieldsValue({
-                date: moment(dataShow.date),
-                time_id: dataShow.time_id,
-                film_id: dataShow.film_id,
-                room_id: dataShow.room_id
-            });
-        }
-    }, [dataShow]);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const onFinish = async (values: any) => {
-        try {
-            values.date = values.date.format("YYYY-MM-DD");
-            await updateShowTime({ ...values, id: dataShow.id });
-            console.log(values);
-            
-            message.success("Cập nhật sản phẩm thành công");
+  const [updateShowTime] = useUpdateShowTimeMutation();
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const { Option } = Select;
+  const { data: films } = useFetchProductQuery();
+  const { data: times } = useFetchTimeQuery();
+  useEffect(() => {
+    if (dataShow) {
+      form.setFieldsValue({
+        date: moment(dataShow.date),
+        time_id: dataShow.time_id,
+        film_id: dataShow.film_id,
+        room_id: dataShow.room_id,
+      });
+    }
+  }, [dataShow]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onFinish = async (values: any) => {
+    try {
+      values.date = values.date.format("YYYY-MM-DD");
+      await updateShowTime({ ...values, id: dataShow.id });
+      console.log(values);
+      message.success("Cập nhật sản phẩm thành công");
 
-            await new Promise((resolve) => setTimeout(resolve, 5000));
+      await new Promise((resolve) => setTimeout(resolve, 5000));
 
-            navigate("/admin/show");
-        } catch (error) {
-            message.error("Cập nhật sản phẩm thất bại");
-        }
-    };
-    const [open, setOpen] = useState(false);
+      navigate("/admin/show");
+    } catch (error) {
+      message.error("Cập nhật sản phẩm thất bại");
+    }
+  };
+  const [open, setOpen] = useState(false);
 
-    const showDrawer = () => {
-        setOpen(true);
-    };
+  const showDrawer = () => {
+    setOpen(true);
+  };
 
-    const onClose = () => {
-        setOpen(false);
-    };
+  const onClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <>
-            <Button onClick={showDrawer}>
-                <div className="flex ">
-                    <EditOutlined />
-                </div>
-            </Button>
+  return (
+    <>
+      <Button onClick={showDrawer}>
+        <div className="flex ">
+          <EditOutlined />
+        </div>
+      </Button>
 
-            <Drawer
-                title="Thêm Rạp Chiếu"
-                width={720}
-                onClose={() => {
-                    onClose();
-                }}
-                open={open}
-                style={{
-                    paddingBottom: 80,
-                }}
-                extra={
-                    <Space>
-                        <Button onClick={onClose}>Hủy</Button>
+      <Drawer
+        title="Cập nhật Suất Chiếu"
+        width={720}
+        onClose={() => {
+          onClose();
+        }}
+        open={open}
+        style={{
+          paddingBottom: 80,
+        }}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Hủy</Button>
 
-                        <Button
-                            danger
-                            type="primary"
-                            htmlType="submit"
-                            onClick={() => {
-                                form.validateFields().then((values) => {
-                                    onFinish(values);
-                                });
-                            }}
-                        >
-                            Cập Nhật
-                        </Button>
-                    </Space>
-                }
+            <Button
+              danger
+              type="primary"
+              htmlType="submit"
+              onClick={() => {
+                form.validateFields().then((values) => {
+                  onFinish(values);
+                });
+              }}
             >
-                <Form
-                    form={form}
-                    layout="vertical"
-                    hideRequiredMark
-                    onFinish={onFinish}
-                >
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item
-                                name="film_id"
-                                label="Phim"
-                                rules={[{ required: true, message: "Vui lòng Chọn Phim" }]}
-                            >
-                                <Select placeholder="Vui lòng Chọn Phim">
-                                    {
-                                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                        (films as any)?.data?.map((time: IFilms, index: number) => {
-                                            return (<Option key={index} value={time.id}> {time.name} </Option>)
-                                        })
-                                    }
-                                </Select>
-                            </Form.Item>
-                        </Col>
+              Cập Nhật
+            </Button>
+          </Space>
+        }
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          hideRequiredMark
+          onFinish={onFinish}
+        >
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="film_id"
+                label="Phim"
+                rules={[{ required: true, message: "Vui lòng Chọn Phim" }]}
+              >
+                <Select placeholder="Vui lòng Chọn Phim">
+                  {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    (films as any)?.data?.map((time: IFilms, index: number) => {
+                      return (
+                        <Option key={index} value={time.id}>
+                          {" "}
+                          {time.name}{" "}
+                        </Option>
+                      );
+                    })
+                  }
+                </Select>
+              </Form.Item>
+            </Col>
 
-                        <Col span={12}>
-                            <Form.Item
-                                name="time_id"
-                                label="Thời Gian"
-                                rules={[{ required: true, message: "Vui lòng Chọn Thời Gian" }]}
-                            >
-                                <Select placeholder="Vui lòng Chọn Thời Gian">
-                                    {
-                                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                        (times as any)?.data?.map((time: ITime, index: number) => {
-                                            return (<Option key={index} value={time.id}> {time.time} </Option>)
-                                        })
-                                    }
-                                </Select>
-                            </Form.Item>
-                        </Col>
-                    </Row>
+            <Col span={12}>
+              <Form.Item
+                name="time_id"
+                label="Thời Gian"
+                rules={[{ required: true, message: "Vui lòng Chọn Thời Gian" }]}
+              >
+                <Select placeholder="Vui lòng Chọn Thời Gian">
+                  {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    (times as any)?.data?.map((time: ITime, index: number) => {
+                      return (
+                        <Option key={index} value={time.id}>
+                          {" "}
+                          {time.time}{" "}
+                        </Option>
+                      );
+                    })
+                  }
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
 
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item
-                                name="date"
-                                label="Ngày Chiếu"
-                                rules={[
-                                    { required: true, message: "Please choose the release date" },
-                                ]}
-                            >
-                                <DatePicker />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item
-                                name="room_id"
-                                label="Phòng Chiếu"
-                                rules={[{ required: true, message: "Vui Lòng Chọn Phòng Chiếu" }]}
-                            >
-                                <Select placeholder="Vui Lòng Chọn Phòng Chiếu">
-                                    <Option value="1">1</Option>
-                                    <Option value="2">2</Option>
-                                </Select>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
-            </Drawer>
-        </>
-    );
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="date"
+                label="Ngày Chiếu"
+                rules={[
+                  { required: true, message: "Please choose the release date" },
+                ]}
+              >
+                <DatePicker />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="room_id"
+                label="Phòng Chiếu"
+                rules={[
+                  { required: true, message: "Vui Lòng Chọn Phòng Chiếu" },
+                ]}
+              >
+                <Select placeholder="Vui Lòng Chọn Phòng Chiếu">
+                  <Option value="1">1</Option>
+                  <Option value="2">2</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Drawer>
+    </>
+  );
 };
 
 export default EditShow;
