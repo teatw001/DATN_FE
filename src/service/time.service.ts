c93f1b16f268903a0703642348122b9e39b03b8e
teatw001 <duydhph26793@fpt.edu.vn>
2023-10-20 01:16:16 +0700

    ...

--- src/service/time.service.ts
+++ src/service/time.service.ts
@@ -1,63 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { ITime } from "../interface/model";
 const timesAPI = createApi({
-    reducerPath: "times",
-    baseQuery: fetchBaseQuery({
-      baseUrl: "http://127.0.0.1:8000/api",
-    }),
-    tagTypes: ["time"],
-    endpoints: (builder) => ({
-      fetchTime: builder.query<ITime[], void>({
-        query: () => "/time/",
-        providesTags: ["time"],
-      }),
-      getTimeById: builder.query<ITime, number | string>({
-        query: (id) => `/time/${id}`,
-        providesTags: ["time"],
-      }),
-      removeTime: builder.mutation({
-        query: (id) => ({
-          url: "/time/" + id,
-          method: "DELETE",
-        }),
-        invalidatesTags: ["time"],
-      }),
-      addTime: builder.mutation({
-        query: (time: ITime) => ({
-          url: "/time/",
-          method: "POST",
-          body: time,
-        }),
-        invalidatesTags: ["time"],
-      }),
-      updateTime: builder.mutation({
-        query: (time: ITime) => ({
-          url: `/time/${time.id}`,
-          method: "PATCH",
-          body: time,
-        }),
-        invalidatesTags: ["time"],
-      }),
-    }),
-  });
-  export const {
-    useFetchTimeQuery,
-    useGetTimeByIdQuery,
-    useRemoveTimeMutation,
-    useAddTimeMutation,
-    useUpdateTimeMutation
-  } = timesAPI;
-  export default timesAPI;
-import {  ITime } from "../interface/model";
-
-const TimeAPI = createApi({
-  reducerPath: "time",
+  reducerPath: "times",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://127.0.0.1:8000/api",
   }),
   tagTypes: ["time"],
   endpoints: (builder) => ({
-    getTime: builder.query<ITime[], void>({
+    fetchTime: builder.query<ITime[], void>({
       query: () => "/time/",
       providesTags: ["time"],
     }),
@@ -91,10 +41,10 @@ const TimeAPI = createApi({
   }),
 });
 export const {
-  useAddTimeMutation,
+  useFetchTimeQuery,
   useGetTimeByIdQuery,
-  useGetTimeQuery,
   useRemoveTimeMutation,
+  useAddTimeMutation,
   useUpdateTimeMutation,
-} = TimeAPI;
-export default TimeAPI;
+} = timesAPI;
+export default timesAPI;
