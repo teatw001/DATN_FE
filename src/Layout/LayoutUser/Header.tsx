aa2d2b046e177de89ee3e4f6c102863b2e99b10f
Ducvpop <ducnhph22018@fpt.edu.vn>
2023-11-18 22:07:00 +0700

    update

--- src/Layout/LayoutUser/Header.tsx
+++ src/Layout/LayoutUser/Header.tsx
@@ -6,7 +6,12 @@ import { ICinemas } from "../../interface/model";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedCinema } from "../../components/CinemaSlice/selectedCinemaSlice";
 import { Modal } from "antd";
-
+import {
+  useFetchProductQuery,
+  useGetFilmCinemeByIdQuery,
+} from "../../service/films.service";
+import { useFetchCateDetailQuery } from "../../service/catedetail.service";
+import { useFetchCateQuery } from "../../service/cate.service";
 interface Option {
   value: string;
   label: string;
@@ -19,12 +24,59 @@ const Header = () => {
   const selectedCinema = useSelector((state: any) => state.selectedCinema);
   const user = useSelector((state: any) => state.auth?.token);
   const { data: cinemas } = useFetchCinemaQuery();
-  const [cinemaOptions, setCinemaOptions] = useState<Option[]>([]);
-  const [isModalVisible, setIsModalVisible] = useState(true);
+  const { data: listCateDetail } = useFetchCateDetailQuery();
+  const { data: cateList } = useFetchCateQuery();
+  const [movies, setMovies] = useState<any>([]);
+  console.log("🚀 ~ file: Header.tsx:26 ~ Header ~ movies:", movies);
+  const [matchingNames, setMatchingNames] = useState([]);
 
+  const [search, setSearch] = useState<string>("");
+  const [cinemaOptions, setCinemaOptions] = useState<Option[]>([]);
+  // console.log("🚀 ~ file: Header.tsx:26 ~ Header ~ cinemaOptions:", cinemaOptions)
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const navigate = useNavigate();
+  const { data: filmCinemaData } = useGetFilmCinemeByIdQuery(
+    selectedCinema // Sử dụng selectedCinema làm tham số
+  );
+  const { data: films } = useFetchProductQuery();
+  console.log("🚀 ~ file: Header.tsx:33 ~ Header ~ films:", films);
   const handleCancel = () => {
     setIsModalVisible(false); // Đóng modal
   };
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.trim() === "") {
+      setSearch("");
+      setMatchingNames([]);
+      return;
+    }
+    setSearch(e.target.value);
+    const matches = movies.filter((movie: any) =>
+      movie.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setMatchingNames(matches);
+  };
+
+  useEffect(() => {
+    if (films) {
+      setMovies((films as any).data);
+    }
+  }, [films]);
+
+  const [dropdownActive, setDropDownActive] = useState(false);
+  const [filmItem, setFilmItem] = useState(null);
+  const [selectCategory, setSelectCategory] = useState<any>(null);
+  console.log(
+    "🚀 ~ file: Header.tsx:93 ~ Header ~ selectCategory:",
+    selectCategory
+  );
+
+  const handleGetFilms = (id: string) => {
+    const filmItem = movies.filter((film: any) => {
+      return film.id === id;
+    });
+    setFilmItem(filmItem);
+  };
+
   useEffect(() => {
     if (cinemas) {
       const cinemaData = (cinemas as any)?.data?.map((cinema: ICinemas) => ({
@@ -64,14 +116,68 @@ const Header = () => {
           <Link to={"/"}>
             <img srcSet="/logo.png/" alt="" />
           </Link>
-          <Cascader
+          {/* <Cascader
             options={cinemaOptions}
             placeholder={"Beta Thanh Xuân"}
             expandTrigger="hover"
             displayRender={displayRender}
             onChange={onChange}
             value={selectedCinema}
-          />
+          /> */}
+          <div className="relative p-3 bg-transparent rounded cursor-pointer w-[260px]">
+            <span
+              onClick={() => {
+                setDropDownActive(!dropdownActive), setFilmItem(null);
+              }}
+              className="w-full max-w-[400px] truncate"
+            >
+              {selectCategory ? selectCategory.name : "tìm kiếm sản phẩm"}
+            </span>
+            {dropdownActive && (
+              <div className="top-[calc(100%_+_8px)] z-10 absolute left-0 w-full bg-gray-400 flex gap-1 flex-col">
+                {(films as any)?.data?.map((filmItemData: any) => {
+                  return (
+                    <p
+                      onMouseOver={() => handleGetFilms(filmItemData.id)}
+                      className="py-2 px-4 bg-red-100 truncate"
+                      onClick={() => {
+                        setSelectCategory({
+                          id: filmItemData.id,
+                          name: filmItemData.name,
+                        }),
+                          setDropDownActive(false);
+                        setFilmItem(null);
+                      }}
+                      key={filmItemData.id}
+                    >
+                      {filmItemData.name}
+                    </p>
+                  );
+                })}
+              </div>
+            )}
+            {filmItem && (
+              <div
+                className="top-[calc(100%_+_8px)] z-10 left-[calc(100%_+_8px)] absolute w-[350px] bg-gray-500"
+                onClick={() => {
+                  navigate(`/movie_about/${(filmItem[0] as any).id}`),
+                    setSelectCategory({
+                      id: (filmItem[0] as any).id,
+                      name: (filmItem[0] as any).name,
+                    }),
+                    setFilmItem(null),
+                    setDropDownActive(false);
+                }}
+              >
+                <img
+                  src={(filmItem[0] as any).image}
+                  alt={(filmItem[0] as any).name}
+                  className="h-10 w-10 rounded"
+                />
+                {(filmItem[0] as any).name}
+              </div>
+            )}
+          </div>
           <Link to={"/"} className="text-[#EE2E24] hover:text-[#EE2E24]">
             Home
           </Link>
@@ -104,7 +210,10 @@ const Header = () => {
             <input
               type="text"
               className="bg-transparent border-2 w-full text-[#FFFFFF] border-gray-300 rounded-full pl-8 pr-4 py-3 focus:outline-none focus:border-blue-500"
-              placeholder="Tìm kiếm..."
+              placeholder="Tìm kiếm"
+              name="search"
+              value={search}
+              onChange={(e) => handleOnChange(e)}
             />
 
             <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
@@ -144,6 +253,24 @@ const Header = () => {
             </svg>
           </div>
         </div>
+        <ul className="bg-white">
+          {matchingNames.map((movie: any) => (
+            <li
+              className="flex items-start justify-start gap-3 py-2 px-3 cursor-pointer hover:bg-gray-100"
+              key={movie.id}
+              onClick={() => {
+                navigate(`/movie_about/${movie.id}`);
+              }}
+            >
+              <img
+                src={movie.image}
+                alt={movie.name}
+                className="h-10 w-10 rounded"
+              />
+              {movie.name}
+            </li>
+          ))}
+        </ul>
       </header>
     </>
   );
--- src/pages/Clients/Movies/Movies.tsx
+++ src/pages/Clients/Movies/Movies.tsx
@@ -7,22 +7,24 @@ const Movies = () => {
   const { data } = useFetchProductQuery() as any;
 
   const movieReleases = data?.data.filter((item: any) => {
-    const result = compareDates(item.release_date, item.end_date)
-    return result
-  })  
+    const result = compareDates(item.release_date, item.end_date);
+    return result;
+  });
 
-  const futureMovies = data?.data.filter((item: any) => {
-    const result = compareReleaseDate(item.release_date)
-    return result
-  }).filter((item1: any) => {
-      const currentDate = new Date()
-      const featureMovieDate = new Date()
-      const releaseDate = new Date(item1.release_date)
-      featureMovieDate.setDate(currentDate.getDate() + 10)
-      if (featureMovieDate > releaseDate) {
-        return item1
-      }
+  const futureMovies = data?.data
+    .filter((item: any) => {
+      const result = compareReleaseDate(item.release_date);
+      return result;
     })
+    .filter((item1: any) => {
+      const currentDate = new Date();
+      const featureMovieDate = new Date();
+      const releaseDate = new Date(item1.release_date);
+      featureMovieDate.setDate(currentDate.getDate() + 10);
+      if (featureMovieDate > releaseDate) {
+        return item1;
+      }
+    });
 
   return (
     <>
@@ -47,14 +49,90 @@ const Movies = () => {
         </div>
       </section>
       <section className="max-w-5xl  my-10  mx-auto ">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-5 lg:gap-8">
+          <div>
+            <div>
+              <select
+                name="HeadlineAct"
+                id="HeadlineAct"
+                className="mt-2 w-[250px] rounded-lg border-gray-300 text-gray-700 sm:text-sm h-[50px]"
+              >
+                <option value="">Tìm kiếm</option>
+                <option value="JM">John Mayer</option>
+                <option value="SRV">Stevie Ray Vaughn</option>
+                <option value="JH">Jimi Hendrix</option>
+                <option value="BBK">B.B King</option>
+                <option value="AK">Albert King</option>
+                <option value="BG">Buddy Guy</option>
+                <option value="EC">Eric Clapton</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <select
+              name="HeadlineAct"
+              id="HeadlineAct"
+              className="mt-2 w-[250px] rounded-lg border-gray-300 text-gray-700 sm:text-sm h-[50px] ml-[50px]"
+            >
+              <option value="">Rạp</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div>
+            <select
+              name="HeadlineAct"
+              id="HeadlineAct"
+              className="mt-2 w-[250px] rounded-lg border-gray-300 text-gray-700 sm:text-sm h-[50px] ml-[100px]"
+            >
+              <option value="">Giờ chiếu</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div>
+            <select
+              name="HeadlineAct"
+              id="HeadlineAct"
+              className="mt-2 w-[250px] rounded-lg border-gray-300 text-gray-700 sm:text-sm h-[50px] ml-[150px]"
+            >
+              <option value="">Ngày xem</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div> <a
+              className="inline-block rounded bg-indigo-600 px-12 py-4 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500 ml-[210px] mt-2"
+              href="/login"
+            >
+              Mua Vé
+            </a>
+          </div>
+        </div>
         <div className="movie now playing">
           <h1 className="text-[#FFFFFF] mb-[34px] mt-[66px] mx-auto text-center text-[41px] font-bold">
             Đang chiếu
           </h1>
           <div className="grid grid-cols-4 gap-7">
-            {movieReleases && movieReleases.map((film: IFilms, index: number) => (
-              <FilmShowing key={index} data={film} />
-            ))}
+            {movieReleases &&
+              movieReleases.map((film: IFilms, index: number) => (
+                <FilmShowing key={index} data={film} />
+              ))}
           </div>
           <button className="mx-auto block mb-[67px]">
             <span>
@@ -67,9 +145,10 @@ const Movies = () => {
             Sắp chiếu
           </h1>
           <div className="grid grid-cols-4 gap-7">
-          {futureMovies && futureMovies.map((film: IFilms, index: number) => (
-              <FilmShowing key={index} data={film} />
-            ))}
+            {futureMovies &&
+              futureMovies.map((film: IFilms, index: number) => (
+                <FilmShowing key={index} data={film} />
+              ))}
           </div>
           <button className="mx-auto block mb-[67px]">
             <span>
